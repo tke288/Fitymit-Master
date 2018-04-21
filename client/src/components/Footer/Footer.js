b82444fa38d30a2ba1d4b1edc84aecd246f36c5c
JairoFrey <jayro15@yahoo.com>
2018-04-21 09:40:25 -0500

    Completed footer template

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,9 +1,52 @@
 import React from "react";
 import "./Footer.css";
+import LI from "../../Images/LIicon.png";
+import FB from "../../Images/FBicon.png";
+import TW from "../../Images/TWicon.png";
 
 const Footer = () => (
-  <footer className="footer">
-    <span>Project spittin stuff</span>
+  <footer className="page-footer font-small indigo pt-0">
+    <div className="container">
+      <div className="row pt-5 mb-3 text-center d-flex justify-content-center" id="topHeader">
+
+        <div className="col-md-3 mb-3">
+          <h6 className="text-uppercase font-weight-bold">
+            <a href="#!">About us</a>
+          </h6>
+        </div>
+
+        <div className="col-md-3 mb-3">
+          <h6 className="text-uppercase font-weight-bold">
+            <a href="#!">Meet A Mentor</a>
+          </h6>
+        </div>
+
+        <div className="col-md-3 mb-3">
+          <h6 className="text-uppercase font-weight-bold">
+            <a href="#!">Become a Mentor</a>
+          </h6>
+        </div>
+
+        <div className="col-md-3 mb-3">
+          <h6 className="text-uppercase font-weight-bold">
+            <a href="#!">Contact Us</a>
+          </h6>
+        </div>
+
+      </div>
+      <hr />
+      <div className="row">
+        <div className="col-md-5"></div>
+        <div className="col-md-4"> 
+          <img src={LI} id="icon"/>"  "<img src={FB} id="icon" />"  "<img src={TW} id="icon" />
+        </div>
+        <div className="col-md-4"></div>
+      </div>
+      <div className="row">
+        <div className="col-md-12 d-flex" id="signoff">Fitymit.com</div>
+      </div>
+    </div>
+
   </footer>
 );
 
