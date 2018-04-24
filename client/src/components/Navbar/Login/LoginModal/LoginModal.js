0650d501b6e38829410d3b64e1daedddcf9ea1c7
tke288 <31257759+tke288@users.noreply.github.com>
2018-04-24 16:58:09 -0500

    Updated modal

--- client/src/components/Navbar/Login/LoginModal/LoginModal.js
+++ client/src/components/Navbar/Login/LoginModal/LoginModal.js
@@ -1,36 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from "react";  
 import "./LoginModal.css";
 import Logo from "../../../../Images/Logo.png";
 import Linkedin from "../../../../pages/Linkedin.js"
 
 const LoginModal = props => (
-    
-    <div>
-    <div className="modal-overlay-div" onClick={e=>{
+
+  <div>
+    <div className="modal-overlay-div" onClick={e => {
       e.stopPropagation();
       props.onOverlayClick();
-      }}/>
+    }} />
     <div className="modal-content-div">
       <div className="modal-dialog-div">
-     
-        <img src={Logo} alt="test this bitch"></img>
-        
-          <form>
-            Email:<br />
-            <input type="text" name="email" />
-            Password: <br />
-            <input type="text" name="password" />
-            <br />
-            <br />
-            <input type="submit" value="Connect Passport Here" />
-            <br />
-            <Linkedin />
-          </form>
+        <img src={Logo} alt="testImage"></img>
+        <Linkedin />
+        <a href="http://localhost:3000/create" class="btn btn-link">Dont have an LinkedIn? Click Here To Create An Account</a>
       </div>
     </div>
   </div>
- 
+
 );
 
 export default LoginModal;
